129c6fc333cd9a83324f4c5270b8f549e2c1b978
JayaswiniBaiDhaipule <jayaswinibai@gmail.com>
2025-04-22 12:35:50 +0000

    printf

---
+++ printf.java
@@ -0,0 +1,18 @@
+public class printf{
+    public static void main(String[] args) {
+
+        // printf() --> is a method used to format output
+        // %[flags][width][.precision][specifier-character]
+
+        String name ="Spongebob";
+        char firstLetter = 'S';
+        int age = 30;
+        double height = 60.5;
+        boolean isEmployed = true;
+
+        System.out.printf("Hello %s",name);
+        System.out.println("Your name start with");
+        
+        
+    }
+}
